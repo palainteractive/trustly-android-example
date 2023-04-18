99c39b8df1053b9011ee9d58fe9468788f8a28c3
DanShieldsPala <130673828+danshieldspala@users.noreply.github.com>
2023-04-18 08:54:08 -0400

    updated returnUrl, cancelUrl
    after some testing, found there were side effects caused by updating thiese properties.   msg://return and msg://cancel was found to be the value these properties already had.      Further testing showed that this change mitigates the side-effects.

--- app/src/main/java/net/trustly/paywithmybanksdkdemoandroid/ui/LightBoxActivity.kt
+++ app/src/main/java/net/trustly/paywithmybanksdkdemoandroid/ui/LightBoxActivity.kt
@@ -37,8 +37,8 @@ class LightBoxActivity : AppCompatActivity() {
         /**
          * patch to deal with  "Uncaught ReferenceError: Paywithmybank is not defined"  message in Android system log
          */
-        lightBoxWidget.proceedToChooseAccount()
-        //lightBoxWidget.hybrid( "javascript:PayWithMyBank.proceedToChooseAccount()", "#return", "#cancel")
+        //lightBoxWidget.proceedToChooseAccount()
+        lightBoxWidget.hybrid( "javascript:PayWithMyBank.proceedToChooseAccount()", "msg://return", "msg://cancel")
     }
 
     private fun redirectToScreen(callback: Callback) {
